9c57bd0fac0dd1bf6aea8342bfafc97976ec3fe4
Syed Mohd Mehndi <hulk.syed07@gmail.com>
2024-03-31 13:14:49 +0530

    List sample products

---
+++ src/components/ProductList.js
@@ -0,0 +1,11 @@
+function ProductList() {
+    return (
+        <>
+            <p>Product 1</p>
+            <p>Product 2</p>
+            <p>Product 3</p>
+        </>
+    )
+}
+
+export default ProductList

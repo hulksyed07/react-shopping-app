6b1e46473e33a4d816b252fd9ea77645355a6e91
Syed Mohd Mehndi <hulk.syed07@gmail.com>
2024-03-31 14:11:10 +0530

    function to add product to cart

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,8 +1,13 @@
-function ProductDetail({ name, price }) {
+function ProductDetail({ id, name, price, addProductToCart }) {
+    const handleAddToCart = () => {
+        addProductToCart({ id, name, price });
+    };
+
     return (
         <>
           <h3>{name}</h3>
           <p>Price: {price}</p>
+          <button onClick={handleAddToCart}>Add to Cart</button>
         </>
     )
 }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,16 +1,25 @@
+import { useState } from "react"
 import ProductDetail from "./ProductDetail"
 
 function ProductList() {
     const products = [
-        { name: "Product 1", price: "300 INR" },
-        { name: "Product 2", price: "200 INR" },
-        { name: "Product 3", price: "150 INR" },
+        { id: 1, name: "Baby Diaper", price: "300 INR" },
+        { id: 2, name: "Baby Talcom Powder", price: "200 INR" },
+        { id: 3, name: "Baby Milk Powder", price: "150 INR" },
     ]
+
+    const [cartProducts, setCartProducts] = useState([])
+
+    console.log(cartProducts)
+
+    const addProductToCart = (product) => {
+        setCartProducts((currentCartProducts) => [...currentCartProducts, product.id])
+    }
 
     return (
         <>
             {products.map((product) => {
-                return <ProductDetail key={product.name} {...product} />
+                return <ProductDetail key={product.id} addProductToCart={addProductToCart} {...product} />
             })}
         </>
     )
